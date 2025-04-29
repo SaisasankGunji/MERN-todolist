de66742c6fd4753849474fb6355adec8fa69d476
Sai sasank Gunji <saisasnk.gunji@gmail.com>
2025-04-29 18:00:02 +0530

    MongoDB connection string updated

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,25 @@
+require("dotenv").config(); // Load environment variables at the top
+
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const { taskRouter } = require("./routes/taskRouter");
-const taskModel = require("./models/taskModel");
 const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send({
     status: 200,
     msg: "Welcome to Home",
   });
 });
-app.use(cors());
 
 app.use("/", taskRouter);
 
 mongoose
-  .connect("mongodb://localhost:27017/tasksDB")
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("DB connected");
     app.listen(8000, () => console.log("Server is running at PORT 8000"));
